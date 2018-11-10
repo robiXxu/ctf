3e5a24e382519a023b6ba54b994eb663bfa7d392
Schiriac Robert <schiriac.robert@gmail.com>
2018-11-10 12:07:02 +0200

    added caesar bruteforce

--- google2018/ocr_is_cool/solve.py
+++ google2018/ocr_is_cool/solve.py
@@ -5,7 +5,9 @@ import pytesseract
 import argparse
 import cv2
 import os
-
+import string
+import json
+import re
 
 argp = argparse.ArgumentParser()
 argp.add_argument("-i", "--image", required=True, help="path to image")
@@ -29,10 +31,35 @@ startIndex = text.index("@ = Snoozed tome")
 endIndex = text.index("No recect")
 emailContent = text[startIndex:endIndex]
 emailContent = emailContent.replace("@ = Snoozed tome", "").replace("> Sent", "")
-print emailContent
+
+reg = "ctf {[a-zA-Z0-9]+}"
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+otherChars = [" ", "\n"]
+
+length = len(alphabet)
+L2I = dict(zip(alphabet,range(length)))
+I2L = dict(zip(range(length),alphabet))
 
 
-# cv2.imshow("gray", gray)
-# cv2.imshow("original", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
+def isOneOfTheOtherChars(c):
+  return c in otherChars
+
+def skip(c):
+  return c in string.punctuation or c in string.digits or isOneOfTheOtherChars(c)
+
+chars = json.dumps(emailContent.encode("utf-8")).lower()
+
+for i in range(length):
+  plaintext = ""
+  for c in chars:
+    if skip(c):
+      plaintext += c
+    else:
+      plaintext += I2L[ (L2I[c]-i) % 26 ]
+
+  if plaintext.find("ctf") != -1:
+    print "Caesar Cipher Key is {}".format(i)
+    flag = re.findall(reg, plaintext)[0]
+    print "Solution is {}".format(flag)
